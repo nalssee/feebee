b6939af2611451dab606d833c31ff841e8eb11b6
nalssee <kenjin@sdf.org>
2019-02-03 22:11:38 +0900

    ..

--- feebee/util.py
+++ feebee/util.py
@@ -6,7 +6,7 @@ import statistics as st
 from scipy.stats import ttest_1samp
 from datetime import timedelta
 import numpy as np
-from itertools import zip_longest, accumulate, chain, groupby
+from itertools import zip_longest, accumulate, chain, groupby, product
 import pandas as pd
 import ciso8601
 import io
@@ -36,7 +36,7 @@ def lag(cols, datecol, ns, add1fn=None, max_missings=10_000):
                     while d1 != d2:
                         if cnt >= max_missings:
                             raise ValueError("Too many missings", r1, r2)
-                        r0 = empty(r1)
+                        r0 = _empty(r1)
                         r0[datecol] = d1
                         rs1.append(r0)
                         d1 = add1fn(d1)
@@ -49,9 +49,9 @@ def lag(cols, datecol, ns, add1fn=None, max_missings=10_000):
         rss = [rs1]
         for n in ns:
             if n >= 0:
-                rss.append([empty(r0)] * n + rs1)
+                rss.append([_empty(r0)] * n + rs1)
             else:
-                rss.append(rs1[(-n):] + [empty(r0)] * (-n))
+                rss.append(rs1[(-n):] + [_empty(r0)] * (-n))
 
         result = []
         for xs in zip(*rss):
@@ -90,53 +90,24 @@ def add_date(date, n):
         raise ValueError("Unsupported date format", date)
 
 
-def ols(rs, y, *xs):
+def ols(rs, y, xs, add_constant=False):
+    xs = listify(xs)
     df = pd.DataFrame(rs)
-    return sm.OLS(df[[y]], sm.add_constant(df[list(xs)])).fit()
+    if add_constant:
+        return sm.OLS(df[[y]], sm.add_constant(df[list(xs)])).fit()
+    else:
+        return sm.OLS(df[[y]], df[list(xs)]).fit()
 
 
-def logit(rs, y, *xs):
+def logit(rs, y, xs, add_constant=False):
+    xs = listify(xs)
     df = pd.DataFrame(rs)
-    # disp=0 suppress some infos regarding optimization
-    return sm.Logit(df[[y]], sm.add_constant(df[list(xs)])).fit(disp=0)
+    if add_constant:
+        # disp=0 suppress some infos regarding optimization
+        return sm.Logit(df[[y]], sm.add_constant(df[list(xs)])).fit(disp=0)
+    else:
+        return sm.Logit(df[[y]], df[list(xs)]).fit(disp=0)
 
-
-# no constant version
-def ols1(rs, y, *xs):
-    df = pd.DataFrame(rs)
-    return sm.OLS(df[[y]], df[list(xs)]).fit()
-
-
-# implicit ordering
-def group(rs, key):
-    keyfn = _build_keyfn(key)
-    rs.sort(key=keyfn)
-    return [list(rs1) for _, rs1 in groupby(rs, keyfn)]
-
-
-
-def add(**kwargs):
-    def fn(r):
-        for k, v in kwargs.items():
-            try:
-                r[k] = v(r)
-            except:
-                r[k] = ''
-        return r
-    return fn
-
-
-#
-def addf(**kwargs):
-    def fn(rs):
-        r0 = rs[0]
-        for k, v in kwargs.items():
-            try:
-                r0[k] = v(rs)
-            except:
-                r0[k] = ''
-        return r0
-    return fn
 
 
 def chunk(rs, n, column=None):
@@ -187,6 +158,7 @@ def winsorize(rs, col, limit=0.01):
         |  limit(float): for both sides respectably.
     returns rs
     """
+    rs = [r for r in rs if isnum(r[col])]
     xs = [r[col] for r in rs]
     lower = np.percentile(xs, limit * 100)
     higher = np.percentile(xs, (1 - limit) * 100)
@@ -197,24 +169,19 @@ def winsorize(rs, col, limit=0.01):
             r[col] = lower
     return rs
 
-def truncate(self, col, limit=0.01):
+
+def truncate(rs, col, limit=0.01):
     """Truncate rows that are out of limits
     Args:
         |  col(str): column name
         |  limit(float): for both sides respectably.
     Returns self
     """
-    xs = self[col]
+    rs = [r for r in rs if isnum(r[col])]
+    xs = [r[col] for r in rs]
     lower = np.percentile(xs, limit * 100)
     higher = np.percentile(xs, (1 - limit) * 100)
-    return self.where(lambda r: r[col] >= lower and r[col] <= higher)
-
-
-def empty(r):
-    r0 = {}
-    for c in r:
-        r0[c] = ''
-    return r0
+    return [r for r in rs if r[col] >= lower and r[col] <= higher]
 
 
 def isnum(*xs):
@@ -237,37 +204,8 @@ def stars(pval):
         return "*"
     return ""
 
-# ==================================================================================================
-# Helpers
-# ==================================================================================================
-
-def _build_keyfn(key):
-    " if key is a string return a key function "
-    # if the key is already a function, just return it
-    if hasattr(key, '__call__'):
-        return key
-    colnames = listify(key)
-    # special case
-    if colnames == ['*']:
-        return lambda r: 1
-
-    if len(colnames) == 1:
-        col = colnames[0]
-        return lambda r: r[col]
-    else:
-        return lambda r: [r[colname] for colname in colnames]
-
 
 def listify(x):
-    """
-    Example:
-    >>> listify('a, b, c')
-    ['a', 'b', 'c']
-    >>> listify(3)
-    [3]
-    >>> listify([1, 2])
-    [1, 2]
-    """
     try:
         return [x1.strip() for x1 in x.split(',')]
     except AttributeError:
@@ -275,24 +213,6 @@ def listify(x):
             return list(iter(x))
         except TypeError:
             return [x]
-
-
-
-# ==================================================================================================
-# Backups
-# ==================================================================================================
-def overlap(rs, size, step=1, key=None):
-    if key:
-        xs = group(rs, key)
-        result = []
-        for i in range(0, len(xs), step):
-            result.append(list(chain(*xs[i:i + size])))
-        return result
-    else:
-        result = []
-        for i in range(0, len(rs), step):
-            result.append(rs[i:i + size])
-        return result
 
 
 def readxl(fname, sheets=None, encoding='utf-8', delimiter=None, quotechar='"', newline='\n'):
@@ -326,21 +246,18 @@ def readxl(fname, sheets=None, encoding='utf-8', delimiter=None, quotechar='"', 
                 yield [c.value for c in row]
 
 
+# implicit ordering
+def group(rs, key):
+    keyfn = _build_keyfn(key)
+    rs.sort(key=keyfn)
+    return [list(rs1) for _, rs1 in groupby(rs, keyfn)]
+
+
 def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
     return zip_longest(fillvalue=fillvalue, *args)
-
-
-def avg(rs, col, wcol=None, ndigits=None):
-    if wcol:
-        xs = [r for r in rs if isnum(r[col], r[wcol])]
-        val = np.average([x[col] for x in xs], weights=[x[wcol] for x in xs]) if xs else ''
-    else:
-        xs = [r for r in rs if isnum(r[col])]
-        val = np.average([x[col] for x in xs]) if xs else ''
-    return round(val, ndigits) if ndigits and xs else val
 
 
 def diff(high, low):
@@ -350,65 +267,6 @@ def diff(high, low):
 def ttest(seq, n=3):
     tval, pval = ttest_1samp(seq, 0.0)
     return f'{round(st.mean(seq), n)}{stars(pval)}', round(tval, n)
-
-
-def table1d(rs, m, pncol, retcol):
-    def get(i):
-        return [r for r in rs if r[pncol] == i]
-
-    r = {}
-    for i in range(0, m + 1):
-        r['p' + str(i)] = avg(get(i), retcol, ndigits=3)
-    # diff
-    high = [r[retcol] for r in get(m)]
-    low = [r[retcol] for r in get(1)]
-    v, tval = ttest(diff(high, low))
-    r['diff'] = v
-    r['tval'] = tval
-    yield r
-
-
-def table2d(rs, m, n, pncol1, pncol2, retcol):
-    def get(rs, i, j):
-        return [r for r in rs if r[pncol1] == i and r[pncol2] == j]
-
-    for i in range(0, m + 1):
-        r = {'p': i}
-
-        for j in range(0, n + 1):
-            rs1 = get(rs, i, j)
-            r['p' + str(j)] = avg(rs1, retcol, ndigits=3)
-        high = [r[retcol] for r in get(rs, i, n)]
-        low = [r[retcol] for r in get(rs, i, 1)]
-
-        v, tval = ttest(diff(high, low))
-        r['diff'] = v
-        r['tval'] = tval
-        yield r
-    # difference line
-    r1 = {'p': 'diff'}
-    r2 = {'p': 'tval'}
-    for j in range(0, n + 1):
-        high = [r[retcol] for r in get(rs, m, j)]
-        low = [r[retcol] for r in get(rs, 1, j)]
-        v, tval = ttest(diff(high, low))
-        r1['p' + str(j)] = v
-        r2['p' + str(j)] = tval
-
-    # diff of diff
-    hh = [r[retcol] for r in get(rs, m, n)]
-    hl = [r[retcol] for r in get(rs, m, 1)]
-    lh = [r[retcol] for r in get(rs, 1, n)]
-    ll = [r[retcol] for r in get(rs, 1, 1)]
-
-    v, tval = ttest(diff(diff(hh, hl), diff(lh, ll)))
-
-    r1['diff'] = v
-    r1['tval'] = tval
-    r2['diff'] = ''
-    r2['tval'] = ''
-    yield r1
-    yield r2
 
 
 def mean(cols, ncol=None):
@@ -426,94 +284,82 @@ def mean(cols, ncol=None):
     return fn
 
 
-def portn(n):
-    return lambda rs: chunk(rs, n)
+def avg(rs, col, wcol=None, ndigits=None):
+    if wcol:
+        xs = [r for r in rs if isnum(r[col], r[wcol])]
+        val = np.average([x[col] for x in xs], weights=[x[wcol] for x in xs]) if xs else ''
+    else:
+        xs = [r for r in rs if isnum(r[col])]
+        val = np.average([x[col] for x in xs]) if xs else ''
+    return round(val, ndigits) if ndigits and xs else val
 
 
-def zero_portn(col, n):
-    def fn(rs):
-        zeros = [r for r in rs if r[col] == 0]
-        others = [r for r in rs if r[col] != 0]
-        return [zeros] + chunk(others, n)
-    return fn
+def _num1(rs, c, p):
+    for r in rs:
+        r['pn_' + c] = ''
+
+    rs = [r for r in rs if isnum(r[c])]
+    rs.sort(key=lambda r: r[c])
+    if isinstance(p, int):
+        rss = chunk(rs, p)
+        n = p
+    # p: (n, port_fn)
+    else:
+        if len(p) != 2:
+            raise ValueError("Invalid numbering direction: ", c, p)
+        n, pfn = p
+        rss = pfn(rs)
+    if not (all(rss) and len(rss) == n):
+        raise ValueError("Not enough rows for column: ", c)
+    for i, rs1 in enumerate(rss, 1):
+        for r in rs1:
+            r['pn_' + c] = i
+    return rss
 
 
-# 1 dimensional numbering
-def numbering1(col, port):
-    def fn(rs):
-        rs = [r for r in rs if isnum(r[col])]
-        rs.sort(key=lambda r: r[col])
-        for i, rs1 in enumerate(port(rs), 1):
-            for r in rs1:
-                # you need overall grouping so,
-                r['pn_' + col] = 0
-                yield r
-                r['pn_' + col] = i
-                yield r
-    return fn
-
-
-def numbering2(col1, port1, col2, port2):
-    def fn(rs):
-        rs = [r for r in rs if isnum(r[col1], r[col2])]
-
-        rs.sort(key=lambda r: r[col1])
-        for i, rs1 in enumerate(port1(rs), 1):
-            for r in rs1:
-                r['pn_' + col1] = 0
-                r['pn_' + col2] = 0
-                yield r
-                r['pn_' + col1] = i
-                yield r
-
-        rs.sort(key=lambda r: r[col2])
-        for j, rs2 in enumerate(port2(rs), 1):
-            for r in rs2:
-                r['pn_' + col2] = j
-                yield r
-                r['pn_' + col1] = 0
-                yield r
-
-    return fn
-
-
-def numbering2dep(col1, port1, col2, port2):
-    def fn(rs):
-        rs = [r for r in rs if isnum(r[col1], r[col2])]
-        rs.sort(key=lambda r: r[col1])
-        for i, rs1 in enumerate(port1(rs), 1):
-            for r in rs1:
-                r['pn_' + col1] = 0
-                r['pn_' + col2] = 0
-                yield r
-                r['pn_' + col1] = i
-                yield r
-            rs1.sort(key=lambda r: r[col2])
-            for j, rs2 in enumerate(port2(rs1), 1):
-                for r in rs2:
-                    r['pn_' + col2] = j
-                    yield r
-                    r['pn_' + col1] = 0
-                    yield r
-        yield from rs
-    return fn
-
-def numbering(**kwargs):
-    def fn(rs):
-        pass
-    return fn
-
-
+# numbering dependent
 def numbering_dep(**kwargs):
-    cps = [(c, p) for c, p in kwargs.items()]
     def fn(rs, cps):
         if cps:
             c, p = cps[0]
-            if isinstance(p, int):
-                
-
-            for rs1 in _num1(rs, cps[0]):
+            for rs1 in _num1(rs, c, p):
                 fn(rs1, cps[1:])
-    return fn
+        return rs
+    cps = [(c, p) for c, p in kwargs.items()]
+    return lambda rs: fn(rs, cps)
 
+
+def numbering(**kwargs):
+    def fn(rs, cps):
+        if cps:
+            c, p = cps[0]
+            _num1(rs, c, p)
+            fn(rs, cps[1:])
+        return rs
+    cps = [(c, p) for c, p in kwargs.items()]
+    return lambda rs: fn(rs, cps)
+
+
+def _empty(r):
+    r0 = {}
+    for c in r:
+        r0[c] = ''
+    return r0
+
+
+def _build_keyfn(key):
+    " if key is a string return a key function "
+    # if the key is already a function, just return it
+    if hasattr(key, '__call__'):
+        return key
+    colnames = listify(key)
+    # special case
+    if colnames == ['*']:
+        return lambda r: 1
+
+    if len(colnames) == 1:
+        col = colnames[0]
+        return lambda r: r[col]
+    else:
+        return lambda r: [r[colname] for colname in colnames]
 
