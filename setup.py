7eee9e5bd0f690a39bcd944d0344bd75d5876126
nalssee <kenjin@sdf.org>
2019-02-08 06:17:41 +0900

    ..

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='feebee',
-      version='0.5.9',
+      version='0.5.10',
       description='datawork tools',
       url='https://github.com/nalssee/feebee.git',
       author='nalssee',
